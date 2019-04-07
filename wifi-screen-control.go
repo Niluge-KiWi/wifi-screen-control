24f2b6ee7db650daa8506d4d9f18d1f838cb1f86
Thomas Riccardi <riccardi.thomas@gmail.com>
2019-04-07 23:27:12 +0200

    v2: support station signal threshold
    Useful to switch off screen when station is connected from another
    room (detected via a low signal).

--- wifi-screen-control.go
+++ wifi-screen-control.go
@@ -1,24 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/urfave/cli"
 	"log"
+	"math"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 )
 
-func IsThereAnyBodyOutThere(device string) bool {
+func IsThereAnyBodyOutThere(device string, threshold int) bool {
 	out, err := exec.Command("iw", "dev", device, "station", "dump").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-	devicePresent := strings.Contains(string(out), "Station ")
-	return devicePresent
+	// out: either nothing or:
+	// Station xx:xx:xx:xx:xx:xx (on wlan0)
+	// 	signal:  	-42 dBm
+	// get station "distance" by parsing its signal
+	stationsSignal := make(map[string]int)
+	var currentStation string
+	scanner := bufio.NewScanner(strings.NewReader(string(out)))
+	for scanner.Scan() {
+		switch fields := strings.Fields(scanner.Text()); fields[0] {
+		case "Station":
+			currentStation = fields[1]
+		case "signal:":
+			signal, err := strconv.Atoi(fields[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			stationsSignal[currentStation] = signal
+			fmt.Printf("Found station %v with signal %v dBm\n", currentStation, signal)
+		}
+	}
+	closestStationSignal := math.MinInt64
+	for _, signal := range stationsSignal {
+		if signal > closestStationSignal {
+			closestStationSignal = signal
+		}
+	}
+	// considere station present if signal is higher than threshold
+	stationPresent := closestStationSignal != math.MinInt64 && closestStationSignal >= threshold
+	return stationPresent
 }
 
 func SwitchMonitor(on bool) {
@@ -35,7 +65,7 @@ func SwitchMonitor(on bool) {
 	}
 }
 
-func Loop(device string, pollingInterval time.Duration) {
+func Loop(device string, threshold int, pollingInterval time.Duration) {
 	// first, make sure the monitor is on on exit
 	defer SwitchMonitor(true)
 
@@ -45,11 +75,11 @@ func Loop(device string, pollingInterval time.Duration) {
 
 	// Main loop
 	// but first: we don't know in which state the monitor is, let's force it
-	previousDevicePresent := IsThereAnyBodyOutThere(device)
+	previousDevicePresent := IsThereAnyBodyOutThere(device, threshold)
 	SwitchMonitor(previousDevicePresent)
 
 	for {
-		devicePresent := IsThereAnyBodyOutThere(device)
+		devicePresent := IsThereAnyBodyOutThere(device, threshold)
 
 		if devicePresent != previousDevicePresent {
 			SwitchMonitor(devicePresent)
@@ -79,6 +109,11 @@ func main() {
 			ArgsUsage: "DEVICE",
 			Flags: []cli.Flag{
 				cli.IntFlag{
+					Name:  "signal-threshold, s",
+					Value: -100,
+					Usage: "Signal threshold (in dBm) above it the station is considered present",
+				},
+				cli.IntFlag{
 					Name:  "interval, n",
 					Value: 10,
 					Usage: "Polling interval for the wifi status, in seconds",
@@ -86,10 +121,11 @@ func main() {
 			},
 			Action: func(c *cli.Context) error {
 				device := c.Args().Get(0)
+				signalThreshold := c.Int("signal-threshold")
 				pollingInterval := time.Duration(c.Int("interval")) * time.Second
-				fmt.Printf("Checking wifi AP device %v every %v\n", device, pollingInterval)
+				fmt.Printf("Checking wifi AP device %v (for stations signal>=%v) every %v\n", device, signalThreshold, pollingInterval)
 
-				Loop(device, pollingInterval)
+				Loop(device, signalThreshold, pollingInterval)
 				return nil
 			},
 		},
